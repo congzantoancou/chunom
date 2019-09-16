e66130cadf3bb14f3a17ac7e55fe6efd2bd78fcc
Fiong <congdantoancau@mail.com>
2019-09-17 06:06:57 +0700

    detail-nom change frorm div inline-block to flex

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Hán Tự</title>
+	<title>Chữ Nôm</title>
 	<link rel="stylesheet" href="bootstrap-3.3.7-dist/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="styles.css">
 </head>
